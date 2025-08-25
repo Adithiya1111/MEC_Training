6ad3e990bb3f1b6f6b3c4505d0604ab7bf8b7fb4
Adithiya <spacegenius01@gmail.com>
2025-08-25 12:46:04 +0530

    Update PokemonDAO.java

--- Placement/Servlet/Pokemon List/src/main/java/com/example/PokemonDAO.java
+++ Placement/Servlet/Pokemon List/src/main/java/com/example/PokemonDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class PokemonDAO {
 
-    private static final String URL  = "jdbc:mysql://localhost:3306/pokemon";
+    private static final String URL  = "jdbc:mysql://localhost:3306/pokelist";
     private static final String USER = "root";
     private static final String PASS = "@Root!";
 
