3a65eb9f06c7bdf0f54de9e3f7eea66345c71832
Adithiya <spacegenius01@gmail.com>
2025-08-25 12:47:50 +0530

    Update Pokemon_JSP.sql

--- Placement/Servlet/Pokemon_JSP.sql
+++ Placement/Servlet/Pokemon_JSP.sql
@@ -1,7 +1,7 @@
-create database pokemon;
+create database pokelist;
 
-use pokemon;
+use pokelist;
 
-create table pokemon(id int auto_increment, name varchar(100), rarity varchar(50), primary key(id));
+create table pokelist(id int auto_increment, name varchar(100), rarity varchar(50), primary key(id));
 
-select * from pokemon;
+select * from pokelist;
